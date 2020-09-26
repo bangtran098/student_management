cf9b68514f186d8892fca49f0d0d0235a6b5583e
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-09-27 00:23:33 +0900

    first commit

---
+++ models/student.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(_id, _name, _email, _phone, _idCard, _math, _physics, _chemistry) {
+        this.id = _id;
+        this.name = _name;
+        this.email = _email;
+        this.phone = _phone;
+        this.idCard = _idCard;
+        this.math = _math;
+        this.physics = _physics;
+        this.chemistry = _chemistry;
+    }
+}
